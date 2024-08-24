3a10b04018004aa78fe6f790250835dc78b0c652
nigarsultana12 <nigarsohana@gmail.com>
2024-08-25 00:11:10 +0600

    space dimond pattern add

---
+++ SpaceDimondPattern.java
@@ -0,0 +1,56 @@
+
+import java.util.Scanner;
+
+public class SpaceDimondPattern {
+    public static void main(String[] args) {
+        Scanner inputScanner = new Scanner(System.in);
+
+        System.out.println("Enter Your Number: ");
+        int n = inputScanner.nextInt();
+        inputScanner.close();
+
+
+        int i,j,s;
+
+        for(i=n;i>=1;i--){
+            for(j=1;j<=i;j++){
+                System.out.print("*");
+            }
+
+            if(n-i > 0){
+                for(s=1;s<=(n-i)*2;s++){
+                    System.out.print(" ");
+                }
+            }
+
+
+            for(j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+        //reverse the pattern
+        for(i=1;i<=n;i++){
+            for(j=1;j<=i;j++){
+                System.out.print("*");
+            }
+
+            if(n-i > 0){
+                for(s=1;s<=(n-i)*2;s++){
+                    System.out.print(" ");
+                }
+            }
+
+
+            for(j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+    }
+
+}
