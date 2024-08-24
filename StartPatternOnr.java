3e8e3d3a74ec0898094de7ad8bfe4ab3453df60d
nigarsultana12 <nigarsohana@gmail.com>
2024-08-25 00:22:36 +0600

    input scanner close() added

---
+++ StartPatternOnr.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class StarPatternOne {
+    public static void main(String[] args) {
+        Scanner inpuScanner = new Scanner(System.in);
+        System.out.println("Enter A Number : ");
+
+        int n = inpuScanner.nextInt();
+        inpuScanner.close();
