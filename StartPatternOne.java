e62708334c86063b8783b93fe30a941b41459b02
nigarsultana12 <nigarsohana@gmail.com>
2024-08-27 21:10:28 +0600

    tania

--- StartPatternOne.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-public class StarPatternOne {
-    public static void main(String[] args) {
-        Scanner inpuScanner = new Scanner(System.in);
-        System.out.println("Enter A Number : ");
-
-        int n = inpuScanner.nextInt();
-        inpuScanner.close();
